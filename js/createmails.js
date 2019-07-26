a109920e81701bd890413a90a0aad7ab98d93ec7
Thomas Verniers <thomas.verniers@myel.be>
2019-07-26 11:24:20 +0200

    Update createmails.js

--- js/createmails.js
+++ js/createmails.js
@@ -13,8 +13,8 @@ function createContent(datas){
   console.log('ok inline')
   console.log(datas)
   datas.forEach(function (el){
-    var receiveMailSend = 'Eric piperseel';
-    var receiceMailObject ='Code du coffre-fort';
+    var receiveMailSend = el.nom;
+    var receiceMailObject =el.objet;
     var newMail ='<tr class="newMail" data-read=0 data-toggle="modal" data-target="#mailModal">'+
         '<td><i class="fas fa-envelope"></i> <i class="far fa-star"></i></td>'+
         '<td>'+receiveMailSend+'</td>'+
